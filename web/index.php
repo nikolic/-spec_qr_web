8735c34567d4ae0f99c3e2068bc51e0c03f3beb5
Marko Nikolic <nikolic89@hotmail.com>
2013-09-29 20:15:38 +0200

    Generate qrcode implemented.

--- web/index.php
+++ web/index.php
@@ -2,10 +2,30 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+require_once __DIR__.'/../vendor/phpqrcode/phpqrcode.php';
+
 $app = new Silex\Application();
+
+$images_path = __DIR__."/temp/";
 
 $app->get('/hello', function() {
     return 'Hello!!!';
+});
+
+$app->get('/qrcode', function() {
+
+	$PNG_WEB_DIR = 'temp/';
+	$PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
+
+	if (!file_exists($PNG_TEMP_DIR)){
+		 mkdir($PNG_TEMP_DIR, 777);
+	}
+       
+	$filename = $PNG_WEB_DIR.uniqid().".png";
+
+	QRcode::png('code data text', $filename);
+
+    return $filename;
 });
 
 $app->get('/nikolic', function() use ($app) {
