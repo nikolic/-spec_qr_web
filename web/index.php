42aa301ba9b43b5d97b7666f2baf248606e10715
Marko Nikolic <nikolic89@hotmail.com>
2013-10-02 00:01:43 +0200

    Simple validation.

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 require_once __DIR__.'/../vendor/phpqrcode/phpqrcode.php'; 
 
+
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
@@ -23,13 +24,26 @@ $app->get('/ping', function() use ($app) {
 });
 
 $app->get('/generate_qrcode/{id}', function($id) use ($app){
+    
+    $checkQuery = "SELECT COUNT( * ) AS  `exist` FROM " 
+                    . "`qr_codes` WHERE  `text` =  ? ";
 
-	$PNG_WEB_DIR = 'temp/';
-	$PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
+    $result = $app['db']->fetchAssoc($checkQuery, array($id)); 
+
+    if( $result['exist'] > 0){
+        return $app->json(array('success' => 0, 
+                        'url' => NULL,
+                        'error' => "QRcode already exist!"
+            ));
+    }
+
+    $PNG_WEB_DIR = 'temp/';
+    $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
 
 	if (!file_exists($PNG_TEMP_DIR)){
 		 mkdir($PNG_TEMP_DIR, 777);
 	}
+
     $filename = uniqid().".png";
 	$save_path = $PNG_WEB_DIR.$filename;
 
@@ -40,7 +54,10 @@ $app->get('/generate_qrcode/{id}', function($id) use ($app){
 
     $res = $app['db']->query($sql);	
 
-    return $save_path;
+    //return $save_path;
+    return $app->json(array('success' => 1, 
+                            'url' => $_SERVER["HTTP_HOST"]."/silex/web/".$save_path
+                ));
 });
 
 $app->get('/nikolic', function() use ($app) {
